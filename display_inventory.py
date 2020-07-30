4e5eb93893c52ebc8ba697a8a4fe68085cae0e42
Amanda Maurell <amanda.maurell@gmail.com>
2020-07-30 14:59:51 -0300

    Updated with rjust and ljust string methods

--- display_inventory.py
+++ display_inventory.py
@@ -1,8 +1,8 @@
 def displayInventory(inventory):
-  print("Inventory:")
+  print("Inventory:".center(20,'-'))
   item_total = 0
   for k, v in inventory.items():
-    print( str(k) + ':' + str(v))
+    print( str(k.ljust(12,'.')) + ':' + str(v).rjust(6))
     item_total = item_total + v
   print( 'The total of items is : ' + str(item_total))
 
