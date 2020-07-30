77070c31bcde8efbfcab886d042a0f1fa48dc13b
Amanda Maurell <amanda.maurell@gmail.com>
2020-07-30 14:18:32 -0300

    Picture grid game...Just another exercise

---
+++ picturegrid.py
@@ -0,0 +1,32 @@
+grid = [['.', '.', '.', '.', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['.', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.']]
+
+for i in grid:
+	print(i[0], end='')
+print()	
+
+for j in grid:
+	print(j[1], end='')
+print()
+
+for k in grid:
+	print(k[2], end='')
+print()	
+
+for l in grid:
+	print(l[3], end='')
+print()
+
+for m in grid:
+	print(m[4], end='')
+print()	
+
+for n in grid:
+	print(n[5], end='')
