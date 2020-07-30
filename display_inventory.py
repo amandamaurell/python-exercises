8bfe33fa1a44cb4071738ed64e74f375244f3f68
Amanda Maurell <amanda.maurell@gmail.com>
2020-07-30 14:43:49 -0300

    Display the inventory as a 'list' of items

---
+++ display_inventory.py
@@ -0,0 +1,24 @@
+def displayInventory(inventory):
+  print("Inventory:")
+  item_total = 0
+  for k, v in inventory.items():
+    print( str(k) + ':' + str(v))
+    item_total = item_total + v
+  print( 'The total of items is : ' + str(item_total))
+
+
+def addToInventory(inventory, addedItems):
+  for i in addedItems:
+    if i not in inventory.keys():
+      inventory[i] = 1
+    else:
+      inventory[i] += 1
+  return inventory
+
+
+inv1 = {'gold coin' : 42, 'rope' : 1}
+dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+
+inv1 = addToInventory(inv1, dragonLoot)
+
+displayInventory(inv1)
